fca1360e427c5911146b677adeab3163cee02bcf
Hyoungwoo In <io21in@hanmail.net>
2024-05-22 13:37:49 +0900

    lecture_164

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,17 @@
+import { Fragment } from "react";
+
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
     const events = getAllEvents();
-  return (
-    <div>
-      <EventList items={events} />
-    </div>
+
+    return (
+    <Fragment>
+        <EventsSearch />
+        <EventList items={events} />
+    </Fragment>
   );
 }
 
